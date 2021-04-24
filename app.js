a0b9d0c9012842238482c5bb9578e1e8a2baf75a
Nur-allhi <noorefty1@gmail.com>
2021-04-24 12:32:43 +0600

    food display & taking suggestion data from api

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
-// Fetch data from Api:-
+// Show some suggestion:
+const hereIsYourSuggetion = (catagory) => {
+  const url = `https://www.themealdb.com/api/json/v1/1/${catagory}.php`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+};
+// Suggestion Food data with Array:-
+const clickCatagory = "categories";
+hereIsYourSuggetion(clickCatagory);
+// // Display **Suggestion** Food data on the website
+// const displaySuggestionFoods = (foods) => {
+//   displayClear();
+//   const displayFoodMenu = document.getElementById("display-foods");
+//   foods.forEach((food) => {
+//     const everyColumn = document.createElement("div");
+//     everyColumn.className = "col-xl-3 col-lg-4 col-md-6";
+
+//     const foodsAre = `
+//     <div class="mt-4 card-div">
+//     <div class="card-img-hover img-div">
+//     <img class="food-images card-img-top" src="${food.strMealThumb}" alt="">
+//     <div>
+//     <div class="card-body">
+//    <h6 class="card-title text-center">${food.strMeal}</h6>
+//     </div>
+//     </div>
+//     `;
+//     everyColumn.innerHTML = foodsAre;
+//     displayFoodMenu.appendChild(everyColumn);
+//   });
+// };
+// Fetch Food data from Api:-
 const getFoods = (foodName) => {
   const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${foodName}`;
   fetch(url)
     .then((res) => res.json())
-    // .then((data) => console.log(data.meals));
+
     .then((data) => {
       if (data.meals === null) {
         console.log("sarch another food");
@@ -12,31 +44,28 @@ const getFoods = (foodName) => {
       }
     });
 };
+// Clear The previous sarch result:
+const displayClear = () => {
+  document.getElementById("display-foods").innerHTML = "";
+};
+
 // Taking input from the sarch bar:-
 document.getElementById("sarch-btn").addEventListener("click", () => {
   const foodInput = document.getElementById("sarch-input").value;
   getFoods(foodInput);
 });
-// Enter key se for event
-// document.getElementById("sarch-btn").addEventListener("keyup", (event) => {
-//   if (event.key === 13) {
-//     event.preventDefault();
-//     document.getElementById("sarch-btn").click();
-//   }
-//   const foodInput = document.getElementById("sarch-input").value;
-//   getFoods(foodInput);
-// });
 
-// Display data on the website
+// Display Food data on the website
 const displayFoods = (foods) => {
+  displayClear();
   const displayFoodMenu = document.getElementById("display-foods");
   foods.forEach((food) => {
     const everyColumn = document.createElement("div");
-    everyColumn.className = "col-md-3";
+    everyColumn.className = "col-xl-3 col-lg-4 col-md-6";
 
     const foodsAre = `
-    <div class="mt-4 card-size">
-    <div class="img-hover-zoom img-div">
+    <div class="mt-4 card-div">
+    <div class="card-img-hover img-div">
     <img class="food-images card-img-top" src="${food.strMealThumb}" alt="">
     <div>
     <div class="card-body">
